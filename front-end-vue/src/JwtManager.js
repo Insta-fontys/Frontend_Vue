f2774667aeeac08cd209019081c9b249feafe2b4
Mneijndorff <33960298+Mneijndorff99@users.noreply.github.com>
2021-11-10 15:59:06 +0100

    Create JwtManager.js

---
+++ front-end-vue/src/JwtManager.js
@@ -0,0 +1,29 @@
+export default{
+
+//Get claims from JWT
+    parseJwt (token) {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    
+        return JSON.parse(jsonPayload);
+    },
+    
+    //Check expiration date of JWT
+    checkExpiration (exp) {
+        const expDate = new Date(exp * 1000)
+        const currentDate = new Date()
+    
+        return expDate.getTime() <= currentDate.getTime();
+    },
+
+    //Get Role from JWT
+    getRole(){
+        const claims = this.parseJwt(localStorage.getItem("jwt"))
+        const role = claims["Role"]
+        return role
+    }
+  
+}
