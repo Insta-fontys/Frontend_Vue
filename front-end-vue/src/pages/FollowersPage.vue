faf12f22b8128900eeeeba6753f64e1223c7042e
Max <m.neijndorff1999@gmail.com>
2021-11-29 11:50:18 +0100

    follow functionality added

---
+++ front-end-vue/src/pages/FollowersPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+import api from '../wrappers/FollowWrapper.js'
+import jwtUtil from '../JwtManager.js'
+
+export default {
+    created(){
+        var role = jwtUtil.getRole()
+        console.log("role = " + role)
+        if(role == "creator"){
+            api.getFollowers()
+        }
+        else
+            api.GetFollowings();
+    }
+}
+</script>
---
+++ front-end-vue/src/wrappers/FollowWrapper.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+var baseUrl = "http://localhost:43085"
+
+var apiConfig = {
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+    }
+}
+
+export default {
+    async PostFollow(model){
+        var response;
+
+        try{
+            response = await axios.post(`${baseUrl}/api/Follow`, model, apiConfig) 
+        }
+        catch(error){
+            response = error.response
+        }
+        return response
+    },
+
+    async getFollowers(){
+        var response;
+
+        try{
+            response = await axios.get(`${baseUrl}/api/Follow`, apiConfig) 
+        }
+        catch(error){
+            response = error.response
+        }
+        return response
+    },
+    
+    async GetFollowings(){
+        var response;
+
+        try{
+            response = await axios.get(`${baseUrl}/api/Follow/following`, apiConfig) 
+        }
+        catch(error){
+            response = error.response
+        }
+        return response
+    }
+}
